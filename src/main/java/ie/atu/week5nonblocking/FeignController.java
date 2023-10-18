411982f438fd18e77d1039ffb93fb32217f7ac14
Denis Costello <G00396309@atu.ie>
2023-10-18 10:23:25 +0100

    Injected an instance for feignService, and getmapping for feign

--- src/main/java/ie/atu/week5nonblocking/FeignController.java
+++ src/main/java/ie/atu/week5nonblocking/FeignController.java
@@ -1,4 +1,16 @@
 package ie.atu.week5nonblocking;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FeignController {
+    private final FeignService feignService;
+
+    public FeignController(FeignService feignService){
+        this.feignService=feignService;
+    }
+
+    @GetMapping("/feign")
+
 }
