03f9acc29b9a6e5a8149086d4c115c5ca55f7dc5
Denis Costello <G00396309@atu.ie>
2023-10-18 10:20:29 +0100

    Created POJO to store JSON response

--- src/main/java/ie/atu/week5nonblocking/TodoResponse.java
+++ src/main/java/ie/atu/week5nonblocking/TodoResponse.java
@@ -1,4 +1,21 @@
 package ie.atu.week5nonblocking;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
 public class TodoResponse {
+    @JsonProperty("userId")
+    private int userId;
+
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("title")
+    private int title;
+
+    @JsonProperty("completed")
+    private int completed;
+
+
 }
